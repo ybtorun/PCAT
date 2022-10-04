520804da501f7efff542facb9be2459116d17ab6
Yusuf Büyüktorun <ybuyuktoruntest@gmail.com>
2022-10-04 14:38:43 +0300

    app,package-lock and package.json are updated

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-const fn = function () {
-  console.log('Function working');
-};
-//test
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Photo Name',
+    description: 'Photo Description',
+  };
+  res.send(photo);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
