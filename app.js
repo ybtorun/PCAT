24962a452cdee74e183214493851c11b8c6a8938
Yusuf Büyüktorun <ybuyuktoruntest@gmail.com>
2022-10-05 16:42:00 +0300

    template engine was set

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
-//sample middleware
-// const myLogger = (req, res, next) => {
-//   console.log('Middleware Log 1 ');
-//   next();
-// }
-// app.use(myLogger);
+//Template engine
+app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
 
+//Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 const port = 3000;
